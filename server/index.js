6c3c408e5d49960e8b133563a367cf705f7f20fa
Dakshin R <drathan45@gmail.com>
2021-10-15 17:49:36 -0400

    init server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//ROUTES//
+
+
+
+
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
+  });
+  
