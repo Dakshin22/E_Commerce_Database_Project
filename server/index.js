596529ca427d61761d05b5f62f529bb31ab70069
Dakshin R <drathan45@gmail.com>
2021-11-13 18:01:36 -0500

    more server changes

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.post("/addToCart", async (req, res) => {
   }
 });
 
-//start purchase
+//start purchase/new cart
 /**
  * Post request
  * Needs user name
@@ -63,43 +63,73 @@ app.post("/newPurchase", async (req, res) => {
 app.get("/pastPurchases", async (req, res) => {
   try {
     const { username } = req.body;
-    const pastOrders = await pool.query("SELECT * FROM Pastorder WHERE username = $1", [username]);
-    res.json(pastOrders.rows);
+    const pastPurchase = await pool.query(
+      "SELECT * FROM Pastpurchase WHERE username = $1",
+      [username]
+    );
+    res.json(pastPurchase.rows);
   } catch (error) {
     console.error(error.message);
   }
 });
 
-//get all items
+//get all items available
 app.get("/items", async (req, res) => {
   try {
-    const allItems = await pool.query("SELECT * FROM item");
+    const allItems = await pool.query("SELECT * FROM item LIMIT 10");
     res.json(allItems.rows);
   } catch (error) {
     console.error(error.message);
   }
 });
 
-// })
-
-// updating item value
-app.put("/cart-items/:id", async (req, res) => {
+//checkout
+/**
+ * post request
+ * updates purcdhase table with timestamp and price information
+ * adds row to pastpurchase table
+ */
+app.post("/checkout", async (req, res) => {
   try {
-    const { id } = req.params;
-    const { qty } = req.body;
-    const itemincart = await pool.query(
-      "UPDATE itemincart SET qty = $1 WHERE itemid = $2",
-      [qty, id]
+    const { timestamp } = req.body;
+    const { price } = req.body;
+    const { username } = req.body;
+    const { purchaseid } = req.body;
+    const checkedOutPurchase = await pool.query(
+      "UPDATE Purchase SET date = $1, price=$2, finished = TRUE, WHERE username = $3 AND purchaseid = $4",
+      [timestamp, price, username, purchaseid]
     );
-    res.json(itemincart);
+    const newPastOrder = await pool.query(
+      "INSERT INTO Pastpurchase (pastpurchaseid, username, checkouttime, totalprice) VALUES ($4, $3, $1, $2)",
+      [timestamp, price, username, purchaseid]
+    );
+    res.json(newPastOrder.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// updating item quantity in cart
+app.put("/updateQuantity", async (req, res) => {
+  try {
+    const { itemid } = req.body;
+    const { qty } = req.body;
+    const { username } = req.body;
+    const { purchaseid } = req.body;
+    const itemincart = await pool.query(
+      "UPDATE PurchaseContainsItem SET quantity = $1 WHERE itemid = $2 AND username = $3 AND purchaseid = $4",
+      [qty, itemid, username, purchaseid]
+    );
+    res.json(`Quantity was updated to ${qty}!`);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 // temp cartitem, need to change the query, used to display items in shopping cart
-app.get("/cart-items", async (req, res) => {
+app.get("/itemsInCart", async (req, res) => {
   try {
+    const { username } = req.body;
     const itemsincart = await pool.query("SELECT * from item limit 10");
     res.json(itemsincart.rows);
   } catch (err) {
@@ -107,13 +137,17 @@ app.get("/cart-items", async (req, res) => {
   }
 });
 
+// remove item from cart
 app.delete("/cart-items/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const itemsincart = await pool.query("DELETE FROM item WHERE itemid = $1", [
-      id,
-    ]);
-    res.json(itemsincart.rows);
+    const { username } = req.body;
+    const { purchaseid } = req.body;
+    const itemsincart = await pool.query(
+      "DELETE FROM purchaseContainsItem WHERE itemid = $1 AND purchaseid = $2 AND username = $3",
+      [id, purchaseid, username]
+    );
+    res.json(`Item with id ${id} deleted from Cart`);
   } catch (err) {
     console.error(err.message);
   }
