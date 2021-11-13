50adbdba43a68f05368b5cd9fd9f1e1d075053b3
Dakshin R <drathan45@gmail.com>
2021-11-13 17:13:31 -0500

    more updates to server endpoints

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json()); //req.body
 
-
 //add to cart
 /**
  * Post request
@@ -20,8 +19,8 @@ app.post("/addToCart", async (req, res) => {
     const { purchaseid } = req.body;
     const { itemid } = req.body;
     const newPurchaseEntry = await pool.query(
-      "INSERT INTO OrderContainsItem (username) VALUES($1) (purchaseid) VALUES($2) (itemid) VALUES($3) RETURNING *",
-      [username, purchaseid, itemid]
+      "INSERT INTO PurchaseContainsItem (purchaseid, username, itemid, quantity) VALUES ($1, $2, $3, 1) RETURNING *",
+      [purchaseid, username, itemid]
     );
 
     res.json(newPurchaseEntry.rows);
@@ -30,27 +29,42 @@ app.post("/addToCart", async (req, res) => {
   }
 });
 
-
-//start order
+//start purchase
+/**
+ * Post request
+ * Needs user name
+ * Inserts a new row into purchases table
+ * sets data, price, and finished to null, null, false respectively
+ * Response is just the purchase created in json form.
+ */
 app.post("/newPurchase", async (req, res) => {
   try {
-    const { description } = req.body;
-    const newOrder = await pool.query(
-      "INSERT INTO Purchase (description) VALUES($1) RETURNING *",
-      [description]
+    const { username } = req.body;
+    const deletion = await pool.query(
+      "DELETE FROM Purchase WHERE username = $1",
+      [username]
+    );
+    const newPurchase = await pool.query(
+      "INSERT INTO Purchase (username, date, price, finished) VALUES ($1, NULL, NULL, FALSE) RETURNING *",
+      [username]
     );
 
-    res.json(newTodo.rows[0]);
+    res.json(newPurchase.rows[0]);
   } catch (error) {
     console.error(error.message);
   }
 });
 
-//getr previopus orders
-app.get("/orders", async (req, res) => {
+//get previous purchases
+/**
+ * get requests
+ * gets all past purchases for a user
+ */
+app.get("/pastPurchases", async (req, res) => {
   try {
-    const pastOrders = await pool.query("SELECT * FROM Pastorder");
-    res.json(allTodos.rows);
+    const { username } = req.body;
+    const pastOrders = await pool.query("SELECT * FROM Pastorder WHERE username = $1", [username]);
+    res.json(pastOrders.rows);
   } catch (error) {
     console.error(error.message);
   }
@@ -67,50 +81,47 @@ app.get("/items", async (req, res) => {
 });
 
 // })
- 
-// updating item value
-app.put("/cart-items/:id", async(req, res) => {
 
+// updating item value
+app.put("/cart-items/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { qty } = req.body;
-    const itemincart = await pool.query("UPDATE itemincart SET qty = $1 WHERE itemid = $2", [qty, id]) 
+    const itemincart = await pool.query(
+      "UPDATE itemincart SET qty = $1 WHERE itemid = $2",
+      [qty, id]
+    );
     res.json(itemincart);
-
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
-
+});
 
 // temp cartitem, need to change the query, used to display items in shopping cart
-app.get("/cart-items", async(req, res) => {
-
+app.get("/cart-items", async (req, res) => {
   try {
     const itemsincart = await pool.query("SELECT * from item limit 10");
     res.json(itemsincart.rows);
-
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
-app.delete("/cart-items/:id", async(req, res) => {
-
+app.delete("/cart-items/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const itemsincart = await pool.query("DELETE FROM item WHERE itemid = $1", [id]);
+    const itemsincart = await pool.query("DELETE FROM item WHERE itemid = $1", [
+      id,
+    ]);
     res.json(itemsincart.rows);
-
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 // todo implemenet delete cart item
 // need to know how we'll represent items in cart first
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000");
-  });
-  
+  console.log("server has started on port 5000");
+});
--- server/queries.sql
+++ server/queries.sql
@@ -1,16 +1,56 @@
-CREATE TABLE Item(itemID INT PRIMARY KEY, title TEXT, category TEXT, description TEXT, img TEXT, aisle TEXT, price REAL);
+CREATE TABLE Item
+(
+    itemID INT PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    description TEXT,
+    img TEXT,
+    aisle TEXT,
+    price REAL
+);
 
-CREATE TABLE PurchaseContainsItem(PurchaseID SERIAL, username TEXT, itemID INT NOT NULL , quantity INT,
-PRIMARY KEY(PurchaseID, username, itemID), FOREIGN KEY(PurchaseID, username) REFERENCES
-Purchase(PurchaseID, username), FOREIGN KEY(itemID) REFERENCES Item(itemID));
+CREATE TABLE PurchaseContainsItem
+(
+    PurchaseID SERIAL,
+    username TEXT,
+    itemID INT NOT NULL ,
+    quantity INT,
+    PRIMARY KEY(PurchaseID, username, itemID),
+    FOREIGN KEY(PurchaseID, username) REFERENCES
+Purchase(PurchaseID, username),
+    FOREIGN KEY(itemID) REFERENCES Item(itemID)
+);
 
-							   
-CREATE TABLE Purchase(PurchaseID SERIAL, username TEXT NOT NULL, Price REAL, Date TIMESTAMP, Finished BOOLEAN,
-PRIMARY KEY(PurchaseID, username), FOREIGN KEY(username) REFERENCES users(username) ON DELETE
-CASCADE);
-				  
-				   
-CREATE TABLE users(username TEXT PRIMARY KEY, password TEXT, fname TEXT, lname TEXT, address
-TEXT, DOB DATE);
 
-CREATE TABLE PastPurchase(PastPurchaseID SERIAL, username TEXT NOT NULL, checkoutTime TIMESTAMP, totalPrice  REAL, PRIMARY KEY(PastPurchaseID, username), FOREIGN KEY(PastPurchaseID, username) REFERENCES Purchase(PurchaseID, username)); 
+CREATE TABLE Purchase
+(
+    PurchaseID SERIAL,
+    username TEXT NOT NULL,
+    Price REAL,
+    Date TIMESTAMP,
+    Finished BOOLEAN,
+    PRIMARY KEY(PurchaseID, username),
+    FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE
+);
+
+
+CREATE TABLE users
+(
+    username TEXT PRIMARY KEY,
+    password TEXT,
+    fname TEXT,
+    lname TEXT,
+    address
+        TEXT,
+    DOB DATE
+);
+
+CREATE TABLE PastPurchase
+(
+    PastPurchaseID SERIAL,
+    username TEXT NOT NULL,
+    checkoutTime TIMESTAMP,
+    totalPrice REAL,
+    PRIMARY KEY(PastPurchaseID, username),
+    FOREIGN KEY(PastPurchaseID, username) REFERENCES Purchase(PurchaseID, username)
+); 
