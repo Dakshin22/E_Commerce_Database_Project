9ffdcb68ab4cb6bf76c506e163c7e4b95aa53bc5
Dakshin R <drathan45@gmail.com>
2021-11-20 12:26:17 -0500

    init

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
 
     user: "postgres",
-    password: "ayonbmly",
+    password: "passsword",
     host: "localhost",
     port: 5432,
     database: "DatabaseProject",
