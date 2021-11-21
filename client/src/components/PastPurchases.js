2524e7fe93fdbe4ef5bfcb05455a1f8204f02e27
Scott Upman <scottupman1@gmail.com>
2021-11-20 22:52:50 -0500

    pastPurchase component

---
+++ client/src/components/PastPurchases.js
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Container } from '@mui/material';
+import Axios from 'axios';
+
+const PastPurchases = (props) => 
+{
+    const [pastOrders, setPastOrder] = useState([]);
+
+    useEffect(() => {
+        console.log(props)
+        getpastPurchases();
+      }, []);
+
+    const getpastPurchases = async e =>
+    {       
+        console.log(props.userInfo.username);
+        Axios.post('http://localhost:5000/pastPurchases',
+        {
+            username: props.userInfo.username
+        }
+        )
+        .then((response) => {console.log(response)})
+    }
+    return (
+    <Container maxWidth="md">
+        <TableContainer component = {Paper}>
+            <Table aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Total</TableCell>
+                    </TableRow>
+                </TableHead>
+
+            
+            </Table>
+        </TableContainer>
+    </Container>
+    )
+}
+export default PastPurchases
